075e041d29274e287c1ddc7c2cada639030049c7
Enrique Aguilar <enriqueao96@gmail.com>
2021-09-13 20:33:25 -0500

    feat: resolvers added

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,4 +1,6 @@
 import * as bcrypt from "bcryptjs";
+import * as randomstring from "randomstring";
+import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
 
 export default {
@@ -7,5 +9,13 @@ export default {
     const password = await bcrypt.hash(pwd, 10);
     return await ctx.prisma.updateUser({ data: { password }, where: { token }});
   },
-  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
+  updateUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.updateUser(args),
+  createUser: (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
+    const token = randomstring.generate(8).toUpperCase();
+    data = {
+      token,
+      ...data
+    };
+    return ctx.prisma.createUser(data);
+  },
 };
--- src/resolvers/Types.ts
+++ src/resolvers/Types.ts
@@ -3,5 +3,8 @@ import { Context } from '../utils'
 export default {
     Order: {
         user: ({ id }, args, ctx: Context) => ctx.prisma.order({ id }).user(),
+    },
+    User: {
+        orders: ({ id }, args, ctx: Context) => ctx.prisma.orders({ where: { user: { id } } }),
     }
 }
